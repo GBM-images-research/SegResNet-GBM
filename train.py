c33c64364c122c67fea9d9f010d27692e091e28a
lmannelli <lmannelli@itba.edu.ar>
2024-03-31 23:21:55 -0300

    new update training

--- train.py
+++ train.py
@@ -1,13 +1,24 @@
-
 import torch
 import time
+import os
 import wandb
+import logging
 from monai.losses import DiceLoss
 from monai.metrics import DiceMetric
 from monai.inferers import sliding_window_inference
 from monai.handlers.utils import from_engine
 
 def train_model(model, train_loader, val_loader, config_train, wandb_api_key):
+    best_metric = 1
+    best_metric_epoch = -1
+    epoch_loss_values = []
+
+
+    # use amp to accelerate training
+    scaler = torch.cuda.amp.GradScaler()
+    # enable cuDNN benchmark
+    torch.backends.cudnn.benchmark = True
+
     device = next(model.parameters()).device
 
     loss_function = DiceLoss(smooth_nr=0, smooth_dr=1e-5, squared_pred=True, to_onehot_y=False, sigmoid=True)
@@ -15,10 +26,10 @@ def train_model(model, train_loader, val_loader, config_train, wandb_api_key):
     lr_scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=config_train["max_epochs"])
 
     # WandB initialization
+    logging.info("Logging in WandB")
     wandb.login(key=wandb_api_key)
     wandb.init(project="SegResNet-1", job_type="train", config=config_train)
-
-    best_metric = float('inf')
+    total_start = time.time()
     for epoch in range(config_train["max_epochs"]):
         torch.cuda.empty_cache()
         epoch_start = time.time()
@@ -32,10 +43,21 @@ def train_model(model, train_loader, val_loader, config_train, wandb_api_key):
             step += 1
             inputs, labels = batch_data["image"].to(device), batch_data["label"].to(device)
             optimizer.zero_grad()
-            outputs = model(inputs)
-            loss = loss_function(outputs, labels)
-            loss.backward()
-            optimizer.step()
+            if config_train["use_scaler"]:
+                # with autocast
+                with torch.cuda.amp.autocast():
+                    outputs = model(inputs)
+                    loss = loss_function(outputs, labels)
+                scaler.scale(loss).backward()
+                scaler.step(optimizer)
+                scaler.update()
+            else:
+                # without autocast
+                outputs = model(inputs)
+                loss = loss_function(outputs, labels)
+                loss.backward()
+                optimizer.step()
+
             epoch_loss += loss.item()
 
             print(
@@ -44,22 +66,34 @@ def train_model(model, train_loader, val_loader, config_train, wandb_api_key):
                 f", Step Time: {(time.time() - step_start):.4f}"
             )
 
-            wandb.log({"Loss": loss.item(), "Epoch": epoch})
+            wandb.log({"Loss": loss.item(), "lr": optimizer.param_groups[0]["lr"], "Epoch": epoch})
 
             torch.cuda.empty_cache()
         
         lr_scheduler.step()
-        epoch_loss /= len(train_loader)
+        epoch_loss /= step
+        epoch_loss_values.append(epoch_loss)
         print(f"Epoch {epoch + 1} Average Loss: {epoch_loss:.4f}")
 
         wandb.log({"Epoch_Average_Loss": epoch_loss})
 
         if epoch_loss < best_metric:
             best_metric = epoch_loss
-            print("Saving new best metric model...")
-            torch.save(model.state_dict(), "best_metric_model.pth")
+            best_metric_epoch = epoch + 1
+            best_model_file = os.path.join(config_train["root_dir"], "best_metric_model.pth")
+            torch.save(model.state_dict(), best_model_file)
+            torch.cuda.empty_cache()
+            print("Saved new best metric model")
+
 
         print(f"Time consuming of Epoch {epoch + 1}: {(time.time() - epoch_start):.4f}")
+    # wandb save the best model
+    artifact_name = f"{wandb.run.id}_best_model"
+    at = wandb.Artifact(artifact_name, type="model")
+    at.add_file(best_model_file)
+    wandb.log_artifact(at, aliases=[f"epoch_{epoch}"])
 
+    total_time = time.time() - total_start
+    logging.info(f"Total time: {total_time}")
     wandb.finish()
-    print(f"Training completed, Best Metric: {best_metric:.4f}")
+    print(f"Training completed, Best Metric: {best_metric:.4f} at epoch: {best_metric_epoch}")
